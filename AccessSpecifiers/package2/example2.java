6d32261c1439d07ddb10bfe408ee97789aee1e87
KrishnaMeghana2710 <85273798+KrishnaMeghana2710@users.noreply.github.com>
2023-02-07 14:56:04 +0530

    Update example2.java

--- AccessSpecifiers/package2/example2.java
+++ AccessSpecifiers/package2/example2.java
@@ -2,12 +2,18 @@ package packages1;
 import packages.demo;
 
 
-public class example2 {
+public class example2  extends demo{
 public static void main(String args[]) {
 	demo dm= new demo();
 	dm.print();
-	// dm.print1();
-	// we cannot access this method because it is protected and cannot be accessed outside package
+	
+	//dm.print1();
+	// we cannot access this method because it is protected and cannot be accessed outside package 
+	// create the present class object and try to access
+	
+	example2 exp= new example2();
+	exp.print1();    // this works
+	
 	//dm.print2();
 	//this is a private method and cannot be accessed outside the class
 	
