d56ae35fad0a1067b58505cf1c65ef7ee1a50a5f
KrishnaMeghana2710 <85273798+KrishnaMeghana2710@users.noreply.github.com>
2023-02-07 12:39:39 +0530

    Create example1.java

---
+++ AccessSpecifiers/package1/example1.java
@@ -0,0 +1,13 @@
+package packages;
+
+
+public class example1 {
+	public static void main(String args[]) {
+		demo dm= new demo();
+		dm.print();
+		dm.print1();
+		//dm.print2();
+		// we cannot access the private method in another class
+	}
+
+}
