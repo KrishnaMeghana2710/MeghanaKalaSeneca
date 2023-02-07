7ad88bda641bd757cd8f64ef11b2bf63da6a4ffc
KrishnaMeghana2710 <85273798+KrishnaMeghana2710@users.noreply.github.com>
2023-02-07 14:45:52 +0530

    Update hierarchical.java

--- Inheritance/hierarchical.java
+++ Inheritance/hierarchical.java
@@ -6,26 +6,48 @@ public class hierarchical {
 	Cat kitten= new Cat();
 	puppy.bark();
 	puppy.eat();
-	
 	kitten.meow();
+	kitten.eat();
 	}
 	
-	
-
 }
 class Animal{
+	Animal(){
+		System.out.println("this is the parent class constructor");
+	}
 	void eat() {
 		System.out.println("Parent eating Method");
 	}
 }
 class Dog extends Animal{
+	Dog(){
+	super();
+
+	System.out.println("Dog constructor");
+	}
+	void eat() {
+		
+		super.eat();
+		System.out.println("Dog is eating");
+	}
 	void bark() {
 		System.out.println("Dog is barking");
 		
 	}
 }
 class Cat extends Animal{
+	Cat(){
+		super();
+		System.out.println("Cat constructor");
+		
+	}
+	void eat() {
+		super.eat();
+		System.out.println("Cat is eating");
+		
+	}
 	void meow() {
+		
 		System.out.println("The cat is meowing");
 	}
 }
